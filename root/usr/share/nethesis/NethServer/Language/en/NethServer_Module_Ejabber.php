cf6c49937dc2c9a83899a47fd9055110cc3d4a02
Stephane de Labrusse <stephdl@de-labrusse.fr>
2018-06-26 14:03:04 +0200

    More Settings in PHP Languages

--- root/usr/share/nethesis/NethServer/Language/en/NethServer_Module_Ejabber.php
+++ root/usr/share/nethesis/NethServer/Language/en/NethServer_Module_Ejabber.php
@@ -3,4 +3,13 @@
 $L['Ejabber_Title'] = 'Chat server';
 $L['Ejabber_Description'] = 'Manage chat server';
 $L['Ejabber_Tags'] = 'chat xmpp jabber ejabber';
-$L['status_label'] = 'Enable Ejabber chat server'; 
+$L['status_label'] = 'Enable Ejabber chat server';
+$L['WebAdmin_label'] = 'Administration web Interface';
+$L['Advanced_label'] = 'Advanced settings';
+$L['S2S_label'] = 'Enable federation (S2S)';
+$L['DisplayGroups_label'] = 'Sort users per group name';
+$L['shaperFast_label'] = 'File transfert maximum speed';
+$L['shaperNormal_label'] = 'File transfert normal speed';
+$L['shaperFastMustBeSuperiorThanshaperNormal'] = 'The maximum speed must be superior than the normal speed';
+$L['EjabberdURL'] = 'URL';
+$L['UsersMustBePartOfJabberadmins'] = 'Admin users must be in the jabberadmins group';
