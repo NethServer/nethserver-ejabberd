c458cbac30bd1db699c373f61354a912df21b907
Davide Principi <davide.principi@nethesis.it>
2019-02-21 11:07:55 +0100

    UI read implementation

--- ui/i18n/lang.js
+++ ui/i18n/lang.js
@@ -4,7 +4,7 @@ var LANG_OBJ = {}
 $.get("i18n/locale-" + LANG + ".json", function (data) {
     LANG_OBJ = data
     $(document).trigger("nethserver-loaded");
-});
+}).fail(function(error){console.error(error)});
 
 function _(string) {
     return LANG_OBJ[string]
--- ui/js/app.js
+++ ui/js/app.js
@@ -1,30 +1,344 @@
-$(document).on("nethserver-loaded", function () {
+((function($){
+
+    function applyTranslations() {
+        $('#app-views [i18n]').each(function () {
+            $(this).text(_($(this).attr('i18n')))
+        });
+    }
+
+    function getDashboard() {
+        applyTranslations();
+
+        $('#loader').show();
+        $('#empty').hide();
+        $('#content').hide();
+        
+        // get stats
+        nethserver.exec(
+            ["nethserver-ejabberd/read"],
+            null,
+            null,
+            function (success) {
+                try {
+                    success = JSON.parse(success);
+                    $('#loader').hide();
+                    if(success.configuration.props.status == 'enabled') {
+                        $('#stats-uptime').text(success.status['uptime']);
+                        $('#stats-registered').text(success.status['registered']);
+                        $('#stats-online').text(success.status['online']);
+                        $('#stats-s2sin').text(success.status['s2sin']);
+                        $('#stats-s2sout').text(success.status['s2sout']);
+                        $('#content').show();
+                    } else {
+                        $('#empty').show();
+                    }
+                } catch (e) {
+                    console.error(e);
+                    $('#loader').hide();
+                    $('#content').show();
+                }
+            },
+            function (error) {
+                console.error(error);
+                $('#loader').hide();
+                $('#content').show();
+            }
+        );
+
+        $('#btn_configure').click(function(){
+            window.location = '#/configuration';
+        });
+
+    } // end getDashboard
+
+
+    function getConfiguration() {
+        $('#loader').show();
+        $('#content').hide();
+        applyTranslations();
+
+        $(".bootstrap-switch").bootstrapSwitch({
+            "onText": _('switch_label_on'),
+            "offText": _('switch_label_off')
+        });
+        
+        $(".bootstrap-touchspin").TouchSpin({
+            min: 1,
+            max: 9999,
+            postfix: _('configuration_retention_days')
+        });
+
+        // set default state to collapsed for expandable field section
+        $('.fields-section-header-pf, .pdc-switch-observer').attr('aria-expanded', 'false');
+        $('.fields-section-pf .form-group, .pdc-switch-observer').addClass('hidden');
+        $('.fa.field-section-toggle-pf').removeClass('fa-angle-down');
+
+        // click the field section heading to expand the section
+        $("#config-advanced-options .field-section-toggle-pf").click(function(event){
+          event.preventDefault();
+          $(this).parents(".fields-section-pf").find(".fa").toggleClass("fa-angle-down");
+          $(this).parents(".fields-section-pf").find(".form-group").toggleClass("hidden");
+          if ($(this).parent().attr('aria-expanded') == 'false') {
+            $(this).parent().attr('aria-expanded', 'true');
+          } else {
+            $(this).parent().attr('aria-expanded', 'false');
+          }
+        });
+
+        $('.pdc-switch-observer[data-pdc-switch]').each(function(){
+            var observer = this;
+            var id = $(this).attr('data-pdc-switch');
+            $('#' + id).change(function() {
+                if(this.checked) {
+                    $(observer).removeClass('hidden').attr('aria-expanded', 'true');
+                } else {
+                    $(observer).addClass('hidden').attr('aria-expanded', 'false');
+                }
+            });
+        });
+
+        $('#config-service-switch').on('switchChange.bootstrapSwitch', function(event, state){
+            if(state) {
+                $('#app-views input[id!=config-service-switch]').attr("disabled", false);
+            } else {
+                $('#app-views input[id!=config-service-switch]').attr("disabled", true);
+            }
+        });
+
+        var webadminUrl = 'https://' + window.location.hostname + ':5280/admin/';
+        $('#configuration-webadmin-url').text(webadminUrl).attr('href', webadminUrl);
+
+        nethserver.exec(
+            ["nethserver-ejabberd/read"],
+            null,
+            null,
+            function (success) {
+                $('#loader').hide();
+                try {
+                    var data = JSON.parse(success);
+
+                    var serviceStatus = data['configuration']['props']['status'] == 'enabled';
+                    var webAdminStatus = data['configuration']['props']['WebAdmin'] == 'enabled';
+                    var s2sStatus = data['configuration']['props']['S2S'] == 'enabled';
+                    var modMamStatus = data['configuration']['props']['ModMamStatus'] == 'enabled';
+                    var retentionStatus = data['configuration']['props']['ModMamPurgeDBStatus'] == 'enabled';
+
+                    $('#config-service-switch').attr('checked', serviceStatus).trigger('change');
+                    $('#config-webadmin-switch').attr('checked', webAdminStatus).trigger('change');
+                    $('#config-federation-switch').attr('checked', s2sStatus).trigger('change');
+                    $('#config-archive-switch').attr('checked', modMamStatus).trigger('change');
+                    $('#config-retention-switch').attr('checked', retentionStatus).trigger('change');
+
+                    $('#config-shaperfast-text').val(data['configuration']['props']['ShaperFast']);
+                    $('#config-shapernormal-text').val(data['configuration']['props']['ShaperNormal']);
+                    $('#config-retention-value').val(data['configuration']['props']['ModMamPurgeDBInterval']);
+
+                } catch (e) {
+                    console.error(e);
+                }
+                $('#content').show();
+            },
+            function (error) {
+                console.error(error);
+                $('#loader').hide();
+                $('#content').show();
+            }
+        );
+
+        $('#config-form').submit(function (event) {
+            event.preventDefault();
+
+            $('#config-loader').show()
+
+            // get values
+            var status = $('#config-status').prop("checked") == true ? 'enabled' : 'disabled'
+            var VirtualHost = $('#config-VirtualHost').val()
+
+            // create obj
+            var configObj = {
+                "props": {
+                    "status": status,
+                    "VirtualHost": VirtualHost,
+                },
+                "name": "ejabberd",
+            }
+
+            // validate obj
+            nethserver.exec(
+                ["nethserver-ejabberd/validate"],
+                configObj,
+                null,
+                function (success) {
+                    // define notifications
+                    nethserver.notifications.success = _("configuration_ok")
+                    nethserver.notifications.error = _("configuration_error")
+
+                    // update values
+                    nethserver.exec(
+                        ["nethserver-ejabberd/update"],
+                        configObj,
+                        function (stream) {
+                            console.info("nethserver-ejabberd", stream);
+                        },
+                        function (success) {
+                            $('#config-loader').hide()
+                            getConfig()
+                        },
+                        function (error) {
+                            $('#config-loader').hide()
+                            console.error(error);
+                        }
+                    );
+                },
+                function (error, data) {
+                    $('#config-loader').hide()
+                    var errorData = {};
+                    try {
+                        errorData = JSON.parse(data);
+                        for (var e in errorData.attributes) {
+                            var attr = errorData.attributes[e]
+                            $('#group-' + attr.parameter).addClass('has-error')
+                            $('#error-' + attr.parameter).show()
+                        }
+                    } catch (e) {
+                        console.error(e)
+                    }
+                }
+            );
+        });
+    } // end getConfiguration function
+
+    function getLogs() {
+        var logsAction = 'dump'
+        var process = null
+        applyTranslations();
+
+        // function to handle logs
+        function handleLogs(action) {
+            $('#loader').show();
+            $('#logs-output').hide();
+
+            process = nethserver.readLogs({
+                    "action": action,
+                    "lines": action == 'dump' ? 30 : null,
+                    "mode": "systemd",
+                    "paths": ["ejabberd"]
+                },
+                action == 'follow' ? function (stream) {
+                    $('#loader').hide();
+                    $('#logs-output').show();
+
+                    $('#logs-output').append(stream)
+
+                    document.getElementById(
+                        "logs-output"
+                    ).scrollTop = document.getElementById(
+                        "logs-output"
+                    ).scrollHeight;
+                } : null,
+                function (success) {
+                    $('#loader').hide();
+                    $('#logs-output').show();
+
+                    if (success.length == 0) {
+                        $('#logs-output').text(_("process_terminated"))
+                    } else {
+                        $('#logs-output').text(success)
+                    }
+
+                    document.getElementById(
+                        "logs-output"
+                    ).scrollTop = document.getElementById(
+                        "logs-output"
+                    ).scrollHeight;
+                },
+                function (error) {
+                    $('#loader').hide();
+                    $('#logs-output').show();
+                    $('#logs-output').text(error)
+                },
+                false
+            );
+        }
+
+        // handle click on button
+        $('#follow-btn').click(function () {
+            if (logsAction == "dump") { // follow mode
+                logsAction = 'follow'
+
+                $('#follow-btn').text(_("logs_action_stop"))
+                $('#logs-output').text("")
+                $('#log-file').text("journalctl -u ejabberd -f")
+            } else if (logsAction == "follow") { // dump mode
+                logsAction = 'dump'
+
+                $('#follow-btn').text(_("logs_action_follow"))
+                $('#logs-output').text("")
+                $('#log-file').text("journalctl -u ejabberd")
+            }
+            handleLogs(logsAction)
+        })
+
+        // call method first time
+        handleLogs(logsAction)
+    } // end getLogs function
+
+    function getAbout() {
+        applyTranslations();
+
+        $('#loader').show()
+        $('#content').hide()
+
+        // get app info
+        nethserver.exec(
+            ["system-apps/read"], {
+                action: "info",
+                name: 'nethserver-ejabberd'
+            },
+            null,
+            function (success) {
+                try {
+                    success = JSON.parse(success);
+
+                    $('#app-name').text(success.name)
+                    $('#app-summary').text(success.summary)
+                    $('#app-version').text(success.release.version)
+                    $('#app-web').attr("href", success.homepage)
+                    $('#app-bug').attr("href", success.bugs.url)
+                    $('#app-author').text(success.author.name + ' | ' + success.author.email)
+
+                    $('#loader').hide()
+                    $('#content').show()
+                } catch (e) {
+                    console.error(e)
+                    $('#loader').hide()
+                    $('#content').show()
+                }
+            },
+            function (error) {
+                console.error(error)
+            }
+        );
+    } // end getAbout function
+
     // define app routing
     var app = $.sammy('#app-views', function () {
         this.use('Template');
 
         this.get('#/', function (context) {
-            context.app.swap('');
-            context.render('views/dashboard.html', {})
-                .appendTo(context.$element());
+            this.partial('views/dashboard.html', {}, getDashboard);
         });
 
-        this.get('#/item1', function (context) {
-            context.app.swap('');
-            context.render('views/item1.html', {})
-                .appendTo(context.$element());
+        this.get('#/configuration', function (context) {
+            this.partial('views/configuration.html', {}, getConfiguration);
         });
 
         this.get('#/logs', function (context) {
-            context.app.swap('');
-            context.render('views/logs.html', {})
-                .appendTo(context.$element());
+            this.partial('views/logs.html', {}, getLogs);
         });
 
         this.get('#/about', function (context) {
-            context.app.swap('');
-            context.render('views/about.html', {})
-                .appendTo(context.$element());
+            this.partial('views/about.html', {}, getAbout);
         });
 
         this.before('.*', function () {
@@ -37,183 +351,8 @@ $(document).on("nethserver-loaded", function () {
 
     });
 
-    app.run('#/');
-
-    /* call nethserver notifications */
-    // success - hide notifications after 3 seconds
-    parent.ns.$children[0].notifications.success.show = true
-    parent.ns.$children[0].notifications.success.message = "Your success message"
-    setTimeout(function () {
-        parent.ns.$children[0].notifications.success.show = false;
-    }, 3000)
-
-    // error
-    parent.ns.$children[0].notifications.error.show = true
-    parent.ns.$children[0].notifications.error.message = "Your error message"
-
-    // event
-    parent.ns.$children[0].notifications.event.show = true
-    parent.ns.$children[0].notifications.event.name = "Your event name"
-    parent.ns.$children[0].notifications.event.message = "Your action-name"
-    parent.ns.$children[0].notifications.event.progress = 50
-    /* */
-
-    /* call nethserver api */
-    // read
-    function read(callback) {
-        nethserver.exec(
-            ["nethserver-ejabberd/read"],
-            null,
-            null,
-            function (success) {
-                try {
-                    success = JSON.parse(success);
-                    callback(success)
-                } catch (e) {
-                    callback(e)
-                }
-            },
-            function (error) {
-                callback(error)
-            }
-        );
-    }
-
-
-    // validate
-    function validate(obj, callback) {
-        nethserver.exec(
-            ["nethserver-ejabberd/validate"],
-            obj,
-            null,
-            function (success) {
-                callback(null)
-            },
-            function (error, data) {
-                var errorData = {};
-                try {
-                    errorData = JSON.parse(data);
-                    callback(errorData)
-                } catch (e) {
-                    callback(e)
-                }
-            }
-        );
-    }
-
-    // update
-    function update(obj, callback) {
-        nethserver.exec(
-            ["nethserver-ejabberd/update"],
-            obj,
-            function (stream) {
-                console.info("nethserver-ejabberd", stream);
-            },
-            function (success) {
-                callback(success);
-            },
-            function (error) {
-                callback(error);
-            }
-        );
-    }
-
-    // exec command and get raw output
-    function execCmd(obj, stream, callback) {
-        nethserver.execRaw(
-            ["nethserver-ejabberd/execute"],
-            obj,
-            function (output) {
-                // this is the raw output from command
-                stream(output);
-            },
-            function (success) {
-                callback(success);
-            },
-            function (error) {
-                callback(error);
-            }
-        );
-    }
-
-    // read logs from application
-    function readLogs(obj, stream, callback) {
-        nethserver.readLogs(obj,
-            stream,
-            function (success) {
-                // get logs dump output
-                callback(success);
-            },
-            function (error) {
-                callback(error);
-            },
-            false // not as superuser
-        );
-    }
-
-    read(function (response) {
-        console.log(response);
+    $(document).on("nethserver-loaded", function () {
+        app.run('#/');
     });
 
-
-    var validateObj = {}
-    validate(validateObj, function (response) {
-        // check errors
-        if (response) {
-            console.error(response)
-        }
-
-        // if no errors
-        // update value
-        update(validateObj, function (result) {
-            // if errors
-            parent.ns.$children[0].notifications.error.message = _("edit_error");
-
-            // if no errors
-            parent.ns.$children[0].notifications.success.message = _("edit_ok");
-            setTimeout(function () {
-                parent.ns.$children[0].notifications.success.show = false;
-            }, 3000)
-        })
-    });
-
-    execCmd({
-            action: 'my-action',
-        },
-        function (output) {
-            $('#my-textarea').append(output);
-        },
-        function (result) {
-            console.info(result);
-        }
-    )
-
-    // read logs in follow mode | tail -f /var/log/messages
-    readLogs({
-        "action": "follow",
-        "lines": null,
-        "mode": "systemd",
-        "paths": ["httpd"]
-    }, function (output) {
-        console.log(output)
-    }, function (dump) {
-        console.log(dump)
-    })
-
-    // read logs and dump to output | cat /var/log/messages with last n lines
-    readLogs({
-        "action": "dump",
-        "lines": 20,
-        "mode": "file",
-        "paths": ["/var/log/messages"]
-    }, null, function (dump) {
-        console.log(dump)
-    })
-    /* */
-
-    /* i18n */
-    $('[i18n]').each(function () {
-        $(this).text(_($(this).attr('i18n')))
-    });
-    /* */
-})
+})(jQuery));
